88705a69f70ad812d5507b86da8daba28f2286ff
Neo Nie <nihgwu@live.com>
2019-12-02 13:52:46 +0800

    Updates

---
+++ precache-manifest.30c48c89469152c253eccd52d2e23e6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877a13934b4f9e0d390da41613642577",
+    "url": "/exchange-demo/index.html"
+  },
+  {
+    "revision": "d7054009277dd6bb8cb5",
+    "url": "/exchange-demo/static/js/2.b9b3b3bf.chunk.js"
+  },
+  {
+    "revision": "2732f640bf46c32a8e8b",
+    "url": "/exchange-demo/static/js/main.85e94ec9.chunk.js"
+  },
+  {
+    "revision": "82ec6e1d50b78cbf9da9",
+    "url": "/exchange-demo/static/js/runtime-main.7e82d605.js"
+  }
+]);
